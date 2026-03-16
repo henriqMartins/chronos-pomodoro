2c347d2ed0ebe20f4fde77eee03210223e109a6a
Henrique Alegre <henriquealegre11@gmail.com>
2026-03-16 17:55:57 -0300

    added menu, logo and timer components and styles to make up the app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { Form } from 'lucide-react';
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { CountDown } from './components/CountDown';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/global.css';
 import './styles/theme.css';
@@ -13,7 +15,34 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>Olá, mundo!</Heading>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form'>  
+        <div className='formRow'> 
+          <label htmlFor="input">task</label>
+          <input id='input'type='text' />
+        </div>
+
+        <div className='formRow'> 
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className='formRow'> 
+          <p>Ciclos</p>
+          <p>0 0 0 0</p>
+        </div>
+
+        <div className='formRow'> 
+          <button>Enviar</button>
+        </div>
+
+        </form>
       </Container>
     </>
   );
