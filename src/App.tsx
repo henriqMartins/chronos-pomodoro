9dc56f2a99c3d0c902f785755fe54bfbf5740927
Henrique Alegre <henriquealegre11@gmail.com>
2026-03-23 17:32:44 -0300

    Refactor app structure by removing unused components and integrating MainForm and Home pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,8 @@
-import { Container } from './components/Container';
-import { CountDown } from './components/CountDown';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { Cycles } from './Cycles';
-import { Footer } from './components/Footer';
+import { Home } from './pages/home';
 
 import './styles/global.css';
 import './styles/theme.css';
-import { DefaultInput } from './components/DefaultInput';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
 
-export function App() {  
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className="form">
-          <div className="formRow">
-            <DefaultInput
-              id="meuInput"
-              type="text"
-              labeltext="task"
-              placeholder="Escreve ai"
-            />
-          </div>
-
-          <div className="formRow">
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className="formRow">
-            <Cycles/>
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon/>} color='red'/>
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+export function App() {
+  return <Home />;
 }
---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,32 @@
+import { PlayCircleIcon } from "lucide-react";
+import { DefaultButton } from "../DefaultButton";
+import { DefaultInput } from "../DefaultInput";
+import { Cycles } from "../Cycles";
+
+
+export function MainForm() {
+    return (
+    <form className="form">
+          <div className="formRow">
+            <DefaultInput
+              id="meuInput"
+              type="text"
+              labeltext="task"
+              placeholder="Escreve ai"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <Cycles/>
+          </div>
+
+          <div className="formRow">
+            <DefaultButton icon={<PlayCircleIcon/>} color='red'/>
+          </div>
+        </form>
+    );
+}
