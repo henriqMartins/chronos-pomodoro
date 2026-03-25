b099cb72cd19edbeedaf60d6c6cf74b76e390e18
Henrique Alegre <henriquealegre11@gmail.com>
2026-03-25 16:48:46 -0300

    Add TaskModel and TaskStateModel types for task management

---
+++ src/models/TaskStateModel.ts
@@ -0,0 +1,14 @@
+import type { TaskModel } from "./TaskModel";
+
+export type TaskStateModel = {
+    tasks: TaskModel[];
+    secondsRemaining: number;
+    formattedSecondsRemaining: string;
+    activeTask: TaskModel | null;
+    currentCycle: number; // 1 a 8
+    config: {
+        workTime: number; // em segundos
+        breakTime: number; // em segundos
+        longBreakTime: number; // em segundos
+    }
+}
