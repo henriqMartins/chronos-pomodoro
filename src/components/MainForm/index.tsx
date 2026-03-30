498db94b5081476374f0e3a2e09a48ae391cedd2
Henrique Alegre <henriquealegre11@gmail.com>
2026-03-30 18:02:02 -0300

    Implement form submission handling in MainForm component

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,24 @@ import { PlayCircleIcon } from 'lucide-react';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { Cycles } from '../Cycles';
+import { useRef, useState } from 'react';
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log("ta top");
+  }
   return (
-    <form className="form">
+    <form onSubmit={handleCreateNewTask} className="form">
       <div className="formRow">
         <DefaultInput
           id="meuInput"
           type="text"
           labeltext="task"
           placeholder="Escreve ai"
+          ref={taskNameInput}
         />
       </div>
 
